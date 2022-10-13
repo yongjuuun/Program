6e9881c4416b59f819abc35bd94ac1a6a00ee0e2
drgj <dydwns7936@naver.com>
2022-10-13 22:59:50 +0900

    -

---
+++ src/입출력과사칙연산/T13_10171.java
@@ -0,0 +1,11 @@
+package 입출력과사칙연산;
+// 고양이
+
+public class T13_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ src/입출력과사칙연산/T9_18108.java
@@ -0,0 +1,12 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class T9_18108 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int boolyear = input.nextInt();
+
+        System.out.println(boolyear - 543);
+    }
+}
